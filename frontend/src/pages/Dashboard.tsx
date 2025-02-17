e42b40b7a06c98de5170b30ea94b3e165abd5d23
Shubha Sarkar <sarkarshubha123@gmail.com>
2025-02-17 12:39:27 +0530

    Old components deleted

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,6 +1,5 @@
 import { useRecoilState, useRecoilValue } from "recoil";
-import DisplayModal from "../components/Modal";
-// import Sidebar from "../components/Sidebar";
+
 import {
   authAtom,
   loadingAtom,
@@ -9,82 +8,12 @@ import {
 } from "../store/atoms/atoms";
 
 import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import CardsContainer from "../components-new/CardContainer";
 import { TopBar } from "../components-new/Navbar2";
 import ShareModal from "../components/ShareModal";
 import { Sidebar } from "../components/Sidebar";
 import { useNoteFunctions } from "../store/hooks/noteHooks";
-import { useNavigate } from "react-router-dom";
-
-// export default function Dashboard() {
-//   const loadedNotes = useRecoilValueLoadable(notesSelector);
-
-//   const isAuthenticated = useRecoilValue(authAtom);
-
-//   const [, setIsModalOpen] = useRecoilState(modalAtom);
-
-//   const handleAddNote = () => {
-//     // setSelectedNote(null);
-//     setIsModalOpen(true);
-//   };
-
-//   if (loadedNotes.state === "loading") {
-//     return <div>Finding Notes...</div>;
-//   }
-
-//   if (loadedNotes.state === "hasError") {
-//     return <div>Some error occurred while fetching this notes.</div>;
-//   }
-
-//   if (loadedNotes.state === "hasValue") {
-//     const notes: INotes[] = loadedNotes.contents.notes;
-
-//     if (!notes) {
-//       return <div>No notes found</div>;
-//     }
-//     if (!Array.isArray(notes)) {
-//       console.error("Notes is not an array:", notes);
-//       return <div>No notes found or invalid data structure.</div>;
-//     }
-
-//     return (
-//       <div className="min-h-screen bg-primary flex">
-//         <Sidebar />
-
-//         <div className="flex-1 flex flex-col">
-//           <Navbar isAuthenticated={isAuthenticated} onAddNote={handleAddNote} />
-
-//           <main className="flex-1 p-6 overflow-auto">
-//             {notes.length === 0 ? (
-//               <div className="h-full flex items-center justify-center">
-//                 <button
-//                   onClick={handleAddNote}
-//                   className="btn-primary flex items-center space-x-2"
-//                 >
-//                   <Plus className="w-5 h-5" />
-//                   <span>Add Mind Note</span>
-//                 </button>
-//               </div>
-//             ) : (
-//               <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-//                 {notes.map((note) => (
-//                   <NoteCard
-//                     key={note._id}
-//                     note={note}
-//                     onEdit={() => {}}
-//                   />
-//                 ))}
-//               </div>
-//             )}
-//           </main>
-
-//           <Footer />
-//         </div>
-//         <DisplayModal />
-//       </div>
-//     );
-//   }
-// }
 
 export default function Dashboard() {
   const isAuthenticated = useRecoilValue(authAtom);
@@ -117,7 +46,6 @@ export default function Dashboard() {
         <div className="min-h-screen  flex">
           <Sidebar />
           <CardsContainer />
-          <DisplayModal />
           <ShareModal />
         </div>
       </>
