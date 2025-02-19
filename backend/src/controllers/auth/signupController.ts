564508412b0b0593d243a7034d73fc1dd5b87a72
Shubha Sarkar <sarkarshubha123@gmail.com>
2025-02-20 02:30:37 +0530

    Auto login in after signup added

--- backend/src/controllers/auth/signupController.ts
+++ backend/src/controllers/auth/signupController.ts
@@ -1,9 +1,10 @@
 import { catchAsyncErrors } from "../../middlewares/catchAsyncErrors";
 import User from "../../models/User";
 import ErrorHandler from "../../utills/Error";
+import { setCookie } from "../../utills/setCookie";
 
 export const signUpController = catchAsyncErrors(async (req, res, next) => {
-  console.log("here")
+  console.log("here");
   //take input
   console.log(req.body);
   const { username, password, fullname } = req.body;
@@ -15,7 +16,7 @@ export const signUpController = catchAsyncErrors(async (req, res, next) => {
     return next(new ErrorHandler("User already exist", 400));
   }
   //make an enty
-  console.log("make a enrty")
+  console.log("make a enrty");
   const newUser = new User({
     fullname: fullname,
     username: username,
@@ -23,8 +24,17 @@ export const signUpController = catchAsyncErrors(async (req, res, next) => {
   });
 
   await newUser.save();
+
+  // genrate access token
+  const token = newUser.getAccessToken();
+  if (!token) {
+    return next(new ErrorHandler("Invalid token", 401));
+  }
+
+  //set to cookie
+  setCookie(res, token);
   //send res
-console.log("new user sregisterd")
+  console.log("new user sregisterd");
   res.status(201).json({
     success: true,
     message: "User Signed Up Successfully",
